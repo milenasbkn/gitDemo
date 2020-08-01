c9ad195203168d9d38fb1813b4b5b0a9e47e05e3
milenasbkn <milenasbkn@yahoo.com>
2020-08-01 12:44:37 -0400

    add Todays_Git.java

---
+++ src/TodayS_Git.java
@@ -0,0 +1,9 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class TodayS_Git {
+    public static void main(String[] args) {
+
+        System.out.println("Practicing moving code to sa staging area: gitt add .");
+    }
+
+}
