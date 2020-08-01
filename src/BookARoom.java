7348845325da579d36592e27e04c5639236f787d
milenasbkn <milenasbkn@yahoo.com>
2020-08-01 15:48:30 -0400

    update BookARoom

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class BookARoom {
 
     public static void main(String[] args) {
         System.out.println("What kind of room would you like to book?");
+
+
+        Scanner s=new Scanner(System.in);
+        int roomNum=s.nextInt();
     }
 }
